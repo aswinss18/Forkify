b9185a751211e8a417a62f3cb0a30003ebd20c45
aswinss18 <aswinartography18@gmail.com>
2024-07-11 23:59:11 +0530

    The add new recipe function is created and the first version is created.Still there are some bugs in showing the icon for new recipes

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'af1e15bb-7290-4aeb-b5e1-96b3d74da53e';
+export const MODAL_CLOSE_SEC = 2.5;
--- src/js/views/previewView.js
+++ src/js/views/previewView.js
@@ -5,7 +5,6 @@ class PreviewView extends View {
 
   _generateMarkup() {
     const id = window.location.hash.slice(1);
-
     return `
     <li class="preview">
      <a class="preview__link ${
@@ -17,6 +16,13 @@ class PreviewView extends View {
         <div class="preview__data">
           <h4 class="preview__title">${this._data.title}</h4>
           <p class="preview__publisher">${this._data.publisher}</p>
+          <div class="preview__user-generated ${
+            this._data.key ? '' : 'hidden'
+          }">
+            <svg>
+              <use href="${icons}#icon-user"></use>
+            </svg>
+          </div>
         </div>
      </a>
     </li>
